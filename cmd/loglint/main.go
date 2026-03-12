87908ec28d1a1734db88d694ad29b6fd4addb38b
HACK3R911 <afanasevevgenij2004@gmail.com>
2026-03-13 00:21:28 +0300

    feat: handle other 3 rules and load config from -config flag in main.go

--- cmd/loglint/main.go
+++ cmd/loglint/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/HACK3R911/go-logs-linter/pkg/analyzer"
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
+var configPath = flag.String("config", "", "path to config.yaml file")
+
 func main() {
-	singlechecker.Main(analyzer.NewAnalyzer(analyzer.Settings{}))
+	flag.Parse()
+
+	settings := analyzer.DefaultSettings()
+
+	if configPath != nil && *configPath != "" {
+		cfg, err := analyzer.LoadConfig(*configPath)
+		if err != nil {
+		} else if cfg != nil {
+			settings = *cfg
+		}
+	}
+	singlechecker.Main(analyzer.NewAnalyzer(settings))
 }
--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -8,15 +8,66 @@ import (
 
 	"github.com/HACK3R911/go-logs-linter/pkg/analyzer/detector"
 	"github.com/HACK3R911/go-logs-linter/pkg/analyzer/rules"
+	"github.com/spf13/viper"
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
 	"golang.org/x/tools/go/ast/inspector"
 )
 
 type Settings struct {
-	AllowUppercaseStart bool
-	AllowedPatterns     []string
-	DisallowedPatterns  []string
+	AllowUppercaseStart bool     `yaml:"allow_uppercase_start"`
+	AllowedPatterns     []string `yaml:"allowed_patterns"`
+	DisallowedPatterns  []string `yaml:"disallowed_patterns"`
+	AllowNonEnglish     bool     `yaml:"allow_non_english"`
+	AllowSpecialChars   bool     `yaml:"allow_special_chars"`
+	AllowSensitiveData  bool     `yaml:"allow_sensitive_data"`
+	SensitiveKeywords   []string `yaml:"sensitive_keywords"`
+}
+
+func DefaultSettings() Settings {
+	return Settings{
+		AllowUppercaseStart: false,
+		AllowedPatterns:     []string{},
+		DisallowedPatterns:  []string{},
+		AllowNonEnglish:     false,
+		AllowSpecialChars:   false,
+		AllowSensitiveData:  false,
+		SensitiveKeywords:   []string{"password", "token", "secret", "key", "api_key", "apiKey"},
+	}
+}
+
+func LoadConfig(path string) (*Settings, error) {
+	if path == "" {
+		return nil, nil
+	}
+	v := viper.New()
+	v.SetConfigFile(path)
+	if err := v.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("reading config: %w", err)
+	}
+	var settings Settings
+	if v.IsSet("rules.allow_uppercase_start") {
+		settings.AllowUppercaseStart = v.GetBool("rules.allow_uppercase_start")
+	}
+	if v.IsSet("rules.allowed_patterns") {
+		settings.AllowedPatterns = v.GetStringSlice("rules.allowed_patterns")
+	}
+	if v.IsSet("rules.disallowed_patterns") {
+		settings.DisallowedPatterns = v.GetStringSlice("rules.disallowed_patterns")
+	}
+	if v.IsSet("rules.allow_non_english") {
+		settings.AllowNonEnglish = v.GetBool("rules.allow_non_english")
+	}
+	if v.IsSet("rules.allow_special_chars") {
+		settings.AllowSpecialChars = v.GetBool("rules.allow_special_chars")
+	}
+	if v.IsSet("rules.allow_sensitive_data") {
+		settings.AllowSensitiveData = v.GetBool("rules.allow_sensitive_data")
+	}
+	if v.IsSet("rules.sensitive_keywords") {
+		settings.SensitiveKeywords = v.GetStringSlice("rules.sensitive_keywords")
+	}
+	return &settings, nil
 }
 
 type logLintAnalyzer struct {
@@ -69,6 +120,27 @@ func (a *logLintAnalyzer) checkLogCall(pass *analysis.Pass, call *ast.CallExpr) 
 		diagnostic := a.caseRule.Check(msg.Text, msg.Position)
 		if diagnostic != nil {
 			pass.Report(*diagnostic)
+		}
+
+		if !a.settings.AllowNonEnglish {
+			diagnostic = rules.CheckEnglish(msg.Text, msg.Position)
+			if diagnostic != nil {
+				pass.Report(*diagnostic)
+			}
+		}
+
+		if !a.settings.AllowSpecialChars {
+			diagnostic = rules.CheckSpecialChars(msg.Text, msg.Position)
+			if diagnostic != nil {
+				pass.Report(*diagnostic)
+			}
+		}
+
+		if !a.settings.AllowSensitiveData {
+			diagnostic = rules.CheckSensitiveData(msg.Text, msg.Position, a.settings.SensitiveKeywords)
+			if diagnostic != nil {
+				pass.Report(*diagnostic)
+			}
 		}
 
 		for _, pattern := range a.settings.DisallowedPatterns {
