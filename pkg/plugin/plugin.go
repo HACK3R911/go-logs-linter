314ea619fa8e1bf3711023183a544ba3dc4a27af
HACK3R911 <afanasevevgenij2004@gmail.com>
2026-03-13 02:05:15 +0300

    feat: add golangci-lint plugin

---
+++ pkg/plugin/plugin.go
@@ -0,0 +1,32 @@
+package plugin
+
+import (
+	"github.com/HACK3R911/go-logs-linter/pkg/analyzer"
+	"github.com/golangci/plugin-module-register/register"
+	"golang.org/x/tools/go/analysis"
+)
+
+func init() {
+	register.Plugin("loglint", New)
+}
+
+type LogLintPlugin struct {
+	settings analyzer.Settings
+}
+
+func New(settings any) (register.LinterPlugin, error) {
+	s, err := register.DecodeSettings[analyzer.Settings](settings)
+	if err != nil {
+		return nil, err
+	}
+	return &LogLintPlugin{settings: s}, nil
+}
+
+func (p *LogLintPlugin) BuildAnalyzers() ([]*analysis.Analyzer, error) {
+	analyzer := analyzer.NewAnalyzer(p.settings)
+	return []*analysis.Analyzer{analyzer}, nil
+}
+
+func (p *LogLintPlugin) GetLoadMode() string {
+	return register.LoadModeTypesInfo
+}
